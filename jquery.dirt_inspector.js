ee4d6bfac883106f8e386dba5d06cf9e3dcf944f
Sterling Cobb <sterlingcobb@mail.com>
2011-12-06 16:19:22 -0700

    First commit, created plugin and added license

---
+++ jquery.dirt_inspector.js
@@ -0,0 +1,47 @@
+// Copyright 2011 Sterling Cobb
+// Available under both the GPL and MIT licenses.
+
+// Check to see if jQuery is avalible.
+if (typeof jQuery == 'undefined') throw("jQuery could not be found.");
+
+(function($){
+  $.fn.dirtInspector = function(params){
+
+    // Defaults to be used when extending this jQuery object.
+    var defaults = {
+      ignore_dirty: ":submit",
+      before_unload_message: "Are you sure you want to switch windows without saving (Unsaved data will be lost!)",
+      bind_before_unload: function( object ){
+        object.bind("beforeunload", function(){
+          // Check if you are clicking a submit button
+          return params.before_unload_message;
+        });
+      }
+    }
+
+    // Add default params to this jquery object to be used later.
+    params = $.extend(defaults, params);
+
+    this.each(function(){
+      // This is a single jQuery object of a node.
+      var $node = $( this );
+
+      // If a submit button is clicked, don't worry about dirty form prompt. 
+      $node.children( params.ignore_dirty ).click(function(){
+        params.before_unload_message = null; // null disables the dirty form message.
+      });
+
+      // Override the changes being made in 'blur' instead, bind it to a keypress.
+      $node.children().keyup(function(event){
+        $(event.target).change();
+      });
+
+      // Check for any changes to form elements.
+      $node.children().change(function(event){
+        params.bind_before_unload( $( window ) );
+      })
+    });
+
+    return this; // keep the channing. 
+  }
+})(jQuery);
